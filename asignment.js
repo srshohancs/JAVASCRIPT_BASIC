ad952da9af605fd1524570f93a7511c2b344f12e
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-02-02 19:39:27 +0600

    First Js Folder For Beginner

---
+++ asignment.js
@@ -0,0 +1,21 @@
+// Values and Variables
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).Log their values to the console.
+
+let country = "Bangladesh";
+let continent = "South Asia";
+let population = 169.4;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ script.js
@@ -0,0 +1,19 @@
+// let js = "Amazing";
+
+// if (js === "Amazing") {
+//     alert("Js is so Amazing language to getting a job");
+// }
+
+// console.log(20 + 30 - 10);
+
+// Value and Variables
+console.log('Shohan');
+console.log(2001 - 2024);
+
+let fName = "Shohan";
+lname = "Anika";
+console.log(fName);
+console.log(lname);
+
+let myFirstJob = "SoftWare Engineer";
+console.log(myFirstJob);

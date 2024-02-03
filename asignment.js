fb8d4b9c0b3ff8f575118fbd2ee4102c0c354665
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-02-03 19:29:46 +0600

    Coding Challenge 1 done

--- asignment.js
+++ asignment.js
@@ -1,5 +1,5 @@
-// Values and Variables
-// Declare variables called country, continent and population and assign their values according to your own country (population in millions).Log their values to the console.
+/* Values and Variables
+ Declare variables called country, continent and population and assign their values according to your own country (population in millions).Log their values to the console.*/
 
 let country = "Bangladesh";
 let continent = "South Asia";
@@ -9,8 +9,8 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-// Data Types
-// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.Log the types of isIsland, population, country and language to the console.
+/* Data Types
+ Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.Log the types of isIsland, population, country and language to the console.*/
 
 let isIsland = false;
 let language;

c009e3d597ec02421a99e8dfb916060c566d6b19
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-02-06 11:19:43 +0600

    coding challenge 3 done

--- codingChallenge.js
+++ codingChallenge.js
@@ -1,4 +1,4 @@
-/* 
+/*
     Coding Challenge #1
 Mark and John are trying to compare their BMI (Body Mass Index), which is
 calculated using the formula:
@@ -33,21 +33,53 @@ const markHigherBMI = BMIMark > BMIJohn;
 console.log(markHigherBMI);*/
 
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
 
-/* Write your code below. Good luck! 🙂 */
+// /* Write your code below. Good luck! 🙂 */
 
-if (BMIMark > BMIJohn) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's(${BMIJohn})!`);
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's(${BMIJohn})!`);
+// }
+// else {
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+
+/*
+CHALLENGE #3
+There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+"Dolphins win the trophy" if Dolphins win, or
+
+"Koalas win the trophy" if Koalas win, or
+
+"Both win the trophy" if their average scores are equal.
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+*/
+
+const scoreDolphins = (96 + 108 + 89 / 3);
+const scoreKoalas = (88 + 91 + 110 / 3);
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins win the trophy');
+}
+else if (scoreDolphins < scoreKoalas) {
+    console.log('Koalas win the trophy');
 }
 else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+    console.log('Both win the trophy');
 }
-

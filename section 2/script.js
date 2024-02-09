1dfefbcf91e93fefba216c1a71a4940aee9e4fd7
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-02-09 12:06:16 +0600

    strict mode work apply

---
+++ section 2/script.js
@@ -0,0 +1,23 @@
+//  Activating Strict Mode
+
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can Drive :D');
+
+// const private = 'asa';
+*/
+
+// Functions
+
+function logger() {
+    console.log('My name is Shohan');
+}
+// calling / running / invoking
+logger();
+logger();
+logger();

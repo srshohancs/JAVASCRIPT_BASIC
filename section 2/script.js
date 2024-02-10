47cdaeb06249a73b01b1ac4fb3e202b0dc9d8c0a
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-02-10 09:04:53 +0600

    Arrow Function part Done

--- section 2/script.js
+++ section 2/script.js
@@ -38,13 +38,13 @@ console.log(appleOrangeJuice);
 
 // Function Declarations
 
-/* 
+/*
 function calcAge1(birthYear) {
     const age = 2024 - birthYear;
     return age;
 }
 const s = calcAge1(2001);
-console.log(s);*/
+console.log(s);
 
 // Function Declaretion
 function calcAge1(birthYear) {
@@ -59,3 +59,24 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1999);
 console.log(age2);
+*/
+
+// Fuction Expression
+const calcAge2 = function (birthYear) {
+    return 2024 - birthYear;
+}
+
+// Arrow Functions (One LIne function)
+const calAge3 = birthYear => 2024 - birthYear;
+const age3 = calAge3(2001);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2024 - birthYear;
+    const retirement = 59 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} year's`;
+}
+// console.log(yearsUntilRetirement(2001));
+console.log(yearsUntilRetirement(1999, 'Shohan'));
+console.log(yearsUntilRetirement(2002, 'Anika'));
